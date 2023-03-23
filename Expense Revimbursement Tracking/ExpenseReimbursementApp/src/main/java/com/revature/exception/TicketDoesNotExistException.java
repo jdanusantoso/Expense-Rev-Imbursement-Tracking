b487d1ec0f59d50c40b992451ffc8560e7c95b4f
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-03-23 01:29:39 -0500

    Push

--- Expense Revimbursement Tracking/ExpenseReimbursementApp/src/main/java/com/revature/exception/TicketDoesNotExistException.java
+++
@@ -1,9 +0,0 @@
-package com.revature.exception;
-
-public class TicketDoesNotExistException extends Exception {
-
-    public TicketDoesNotExistException() {
-        super("The ticket you are looking for does not exist.");
-    }
-
-}
